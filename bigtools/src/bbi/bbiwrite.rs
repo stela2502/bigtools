d8369e9139635aae0b944b471d83b4d9944d468c
Stefan Lang <Stefan.Lang@med.lu.se>
2025-05-15 13:10:03 +0200

    State that you drop send.

--- bigtools/src/bbi/bbiwrite.rs
+++ bigtools/src/bbi/bbiwrite.rs
@@ -865,6 +865,7 @@ pub(crate) fn write_vals<
 
     vals_iter.process_to_bbi(&runtime, &mut do_read, &mut advance)?;
 
+    eprintln!("Dropping send now!");
     drop(send);
 
     let summary_complete = summary.unwrap_or(Summary {
@@ -1010,6 +1011,7 @@ pub(crate) fn write_vals_no_zoom<
 
     vals_iter.process_to_bbi(&runtime, &mut do_read, &mut advance)?;
 
+    eprinln!("Dropping send now 2");
     drop(send);
 
     let summary_complete = summary.unwrap_or(Summary {
